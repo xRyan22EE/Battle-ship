647c1d6f53c4076dec1766ba1001c21c09e3a94e
Ryan <152604843+xRyan22EE@users.noreply.github.com>
2024-11-01 16:08:01 +0300

    Update main.py

--- Data/main.py
+++ Data/main.py
@@ -1,18 +1,15 @@
 # module import
-
 import pygame
 
 # module Initialization 
 pygame.init()
 
 # game assets and objects 
-
 class ship:
-    def __init__(self, name, img, pos, size, NumberOfGun = 0, GunPath = None, gunsize = None, guncoordsoffset = None):
+    def __init__(self, name, img, pos, size):
         self.name = name
 
-        # Load the Vertical Imge.
-
+        # Load the Vertical Image.
         self.vimg = LoadImage(img, size)
         self.vimgwidth = self.vimg.get_width()
         self.vimgheight = self.vimg.get_height()
@@ -20,7 +17,6 @@ class ship:
         self.vimgrect.topleft = pos
 
         # Load the Horizontal Image.
-
         self.himg = pygame.transform.rotate(self.vimg, -90)
         self.himgwidth = self.himg.get_width()
         self.himgheight = self.himg.get_height()
@@ -28,8 +24,7 @@ class ship:
         self.himgrect.topleft = pos
 
         # Image and rectangle 
-
-        self.image =  self.vimg
+        self.image = self.vimg
         self.rect = self.vimgrect
         self.rotation = False
     
@@ -38,10 +33,8 @@ class ship:
         window.blit(self.image, self.rect)
 
 # game utility function 
-
 def CreateGameGrid(rows, cols, CellSize, pos):
-    #create game grid with coordinate for each cell 
-
+    # create game grid with coordinate for each cell 
     startx = pos[0]
     starty = pos[1]
     CoordGrid = []
@@ -57,7 +50,7 @@ def CreateGameGrid(rows, cols, CellSize, pos):
 
     return CoordGrid
 
-def UpdateGameLogic(rows, cols ,):
+def UpdateGameLogic(rows, cols):
     # Update the game grid with logic, ie - spaces and X for ships
     game_logice = []
     for row in range(rows):
@@ -70,17 +63,17 @@ def UpdateGameLogic(rows, cols ,):
 
 def ShowGridOnScreen(Window, CellSize, PlayerGrid, ComputerGrid):
     GameGrids = [PlayerGrid, ComputerGrid]
-    for gird in GameGrids:
-        for Row in gird:
+    for grid in GameGrids:
+        for Row in grid:
             for Col in Row:
                 pygame.draw.rect(Window, (255, 255, 255), (Col[0], Col[1], CellSize, CellSize), 1)
 
 def printtest():
-    print(" Player grid ".center(50,"#"))
+    print(" Player grid ".center(50, "#"))
     for i in pGameLogic:
         print(i)
     
-    print(" computer grid ".center(50,"#"))
+    print(" computer grid ".center(50, "#"))
     for i in cGameLogic:
         print(i)
 
@@ -93,15 +86,14 @@ def UpdateGameScreen(window):
     for ship in Playerfleet:
         ship.draw(window)
 
-
     pygame.display.update()
 
-def LoadImage(path, size, rotate = False):
+def LoadImage(path, size, rotate=False):
     # A function to import Images Into memory
     img = pygame.image.load(path).convert_alpha()
-    img = pygame.transform.scale(img,size)
+    img = pygame.transform.scale(img, size)
     if rotate:
-        img = pygame.transform.rotate(img,-90)
+        img = pygame.transform.rotate(img, -90)
     return img
 
 def createfleet():
@@ -111,11 +103,7 @@ def createfleet():
             ship(name, 
             Playerf[name][1],
             Playerf[name][2],
-            Playerf[name][3],
-            Playerf[name][4],
-            Playerf[name][5],
-            Playerf[name][6],
-            Playerf[name][7])
+            Playerf[name][3])
         )
     return fleet
 
@@ -124,54 +112,46 @@ def set_grid_size(new_rows, new_cols):
     global raws, cols, CellSize, pGameGrid, pGameLogic, cGameGrid, cGameLogic
     raws = new_rows
     cols = new_cols
-     # divide ScreenHight by 2 to leave space for both grids
+
+    # divide ScreenHight by 2 to leave space for both grids
     CellSize = min(ScreenWidth // cols, ScreenHight // (2 * raws)) 
     pGameGrid = CreateGameGrid(raws, cols, CellSize, (50, 50))
     pGameLogic = UpdateGameLogic(raws, cols)
-    cGameGrid = CreateGameGrid(raws, cols, CellSize, (((ScreenWidth-50)-(cols * CellSize)), 50))
+    cGameGrid = CreateGameGrid(raws, cols, CellSize, (((ScreenWidth - 50) - (cols * CellSize)), 50))
     cGameLogic = UpdateGameLogic(raws, cols)
 
-
-#  Game Settings and Variables
-
+# Game Settings and Variables
 ScreenWidth = 1260 
 ScreenHight = 960
 
 # colors 
 
 
-# pygame display Initialization 
 
+# pygame display Initialization 
 GameScreen = pygame.display.set_mode((ScreenWidth, ScreenHight))
 pygame.display.set_caption("Battle Ship Demo")
 
 # Game Lists/Dictionaries
-
 Playerf = {
-    "battleship": ["battleship", "images/ships/battleship/battleship.png", (125, 600), (40, 195), 4, "images/ships/battleship/battleshipgun.png", (0.4, 0.125), [-0.525, -0.34, 0.67, 0.49]],
+    "battleship": ["battleship", "images/ships/battleship/battleship.png", (125, 600), (40, 195)],
 
-    "cruiser": ["cruiser", "images/ships/cruiser/cruiser.png", (200, 600), (40, 195), 2, "images/ships/cruiser/cruisergun.png", (0.4, 0.125), [-0.36, 0.64]],
+    "cruiser": ["cruiser", "images/ships/cruiser/cruiser.png", (200, 600), (40, 195)],
 
-    "destroyer": ["destroyer", "images/ships/destroyer/destroyer.png", (275, 600), (30, 145), 2, "images/ships/destroyer/destroyergun.png", (0.5, 0.15), [-0.52, 0.71]],
+    "destroyer": ["destroyer", "images/ships/destroyer/destroyer.png", (275, 600), (30, 145)],
 
-    "patrol boat": ["patrol boat", "images/ships/patrol boat/patrol boat.png", (425, 600), (20, 95), 0, "", None, None],
+    "patrol boat": ["patrol boat", "images/ships/patrol boat/patrol boat.png", (425, 600), (20, 95)],
 
-    "submarine": ["submarine", "images/ships/submarine/submarine.png", (350, 600), (30, 145), 1, "images/ships/submarine/submarinegun.png", (0.25, 0.125), [-0.45]],
+    "submarine": ["submarine", "images/ships/submarine/submarine.png", (350, 600), (30, 145)],
 
-    "carrier": ["carrier", "images/ships/carrier/carrier.png", (50, 600), (45, 245), 0, "", None, None],
+    "carrier": ["carrier", "images/ships/carrier/carrier.png", (50, 600), (45, 245)],
 
-    "rescue ship": ["rescue ship", "images/ships/rescue ship/rescue ship.png", (500, 600), (20, 95), 0, "", None, None]
-
+    "rescue ship": ["rescue ship", "images/ships/rescue ship/rescue ship.png", (500, 600), (20, 95)]
 }
 
 # loading game variables 
-
-
 set_grid_size(10, 10)
-
-
 Playerfleet = createfleet()
-
 Computerfleet = None
 
 printtest()
@@ -180,15 +160,13 @@ printtest()
 
 
 
-
 # Initialise players 
 
 
-# Main game loop 
 
+# Main game loop 
 RunGame = True
 while RunGame:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RunGame = False
