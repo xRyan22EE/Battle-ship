7afdb7e2ddeb3db94ad4442e4fe050c59cfb079f
Ryan <152604843+xRyan22EE@users.noreply.github.com>
2024-11-18 01:36:09 +0300

    new v2

--- Data/grid.py
+++ Data/grid.py
@@ -32,3 +32,22 @@ def CreateGameGrid(rows, cols, CellSize, pos):
     # return the grid list with the coordinates of each cell in the grid
     return CoordGrid
 
+def UpdateGameLogic(rows, cols):
+    # create game logic grid with empty string for each cell
+    game_logice = []
+    # loop through the rows and columns to create the grid
+    for row in range(rows):
+
+        # create a list to hold the value of each cell in a row
+        rowx = []
+
+        # loop through the columns to create the cells in a row
+        for col in range(cols):
+
+            # add the value of the cell to the row list
+            rowx.append(" ")
+
+        # add the row list to the grid list and move to the next row in the grid
+        game_logice.append(rowx)
+        
+    return game_logice
